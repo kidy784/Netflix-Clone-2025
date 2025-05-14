fa3f11fc70ec20071e96f1d5197d02c521f2ee73
kidy784 <kidistsebseba@gmail.com>
2025-05-14 13:46:13 -0700

    banner

---
+++ netflix-clone/src/utils/requests.jsx
@@ -0,0 +1,14 @@
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const request={
+fetchTrending:`trending/all/day?api_key=${API_KEY}&language=en-US`,
+
+fetchNetflixOrginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+fetchTopRateOriginals:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+fetchActionMovies:`/discover/movie?api_key=${API_KEY}&With_genres=28`,
+
+fetchComdyMovies:`/discover/movie?api_key=${API_KEY}&With_genres=35`,
+fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&With_genres=10749`,
+fetchDoucumentaries:`/discover/movie?api_key=${API_KEY}&With_genres=99`,
+fetchTvShow:`tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+}
+export default request;
